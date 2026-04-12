62043e6ffd4ca136b861ac3a768a95eb1c9671b0
Ujwal-kumar-swain-2005 <swainujwalkumar@gmail.com>
2026-04-12 18:54:30 +0530

    remove backend folder

--- backend/src/main/java/com/example/backend/repositories/NexchatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repositories;
-
-import com.example.backend.entities.NexchatConversation;
-import org.hibernate.boot.models.JpaAnnotations;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NexchatRepository extends JpaRepository<NexchatConversation,Long> {
-
-}
